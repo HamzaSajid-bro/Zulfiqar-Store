f5b30b6d24f414fc47e42cdb621710278f72e35d
HamzaSajid-bro <zulfiqar00778866@gmail.com>
2025-09-26 19:50:20 +0500

    Add files via upload

---
+++ components/connect.php
@@ -0,0 +1,18 @@
+2<?php
+$host = 'localhost';
+$db   = 'shop_db';
+$user = 'root';
+$pass = '';      // default for XAMPP is empty
+$charset = 'utf8';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+    $conn = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
